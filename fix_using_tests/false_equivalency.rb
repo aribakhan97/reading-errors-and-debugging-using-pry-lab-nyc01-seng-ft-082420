36f6524932a33184b682ef5e8a79910190631735
Ariba Khan <akhan41@binghamton.edu>
2020-08-03 17:56:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   if num = 1
-     "HAM HAM HAM IN MY TUMMY"
+     "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-      "YUM YUM MUNCH MUNCH MUNCH"
+     "HAM HAM HAM IN MY TUMMY"
   end
 end
 
